0c2e50856abd6c57aeb4ab41dd3dd2d097b06f73
XYZI <113294539+xyzimusic@users.noreply.github.com>
2023-06-04 01:04:07 +0300

    CODE-WARS 03 changes

--- task_04.js
+++ task_04.js
@@ -1,36 +1,17 @@
-//The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+var uniqueInOrder=function(iterable){
+    let result;
+    //your code here - remember iterable can be a string or an array
 
-//Examples
-//"din" => "((("
-//"recede" => "()()()"
-//"Success" => ")())())"
-//"(( @"     =>  "))((" 
-//Notes
-
-//Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
-
-
-
-//========================================================================================================
-
-function duplicateEncode(str){
-    //create a var to hold string value that IGNORES case
-    var word = str.toLowerCase();
-    //create a var to hold the finished string to return after it's looped through 
-    var unique = '';
-    //loop through all letters in string
-      for (var i = 0; i < word.length; i++) {
-    //check for any characters that repeat
-        if (word.lastIndexOf(word[i]) === word.indexOf(word[i])) {
-    //for each character that never dupes, place (
-          unique += '(';
-        } else
-    //for each character that IS a dupe, place )
-        unique += ')';
-      }
-    //return the full string value where '(' are non duped and all')' are duped
-      return unique;
+   iterable.forEach((element,i) => {
     
+    if(iterable.lastIndexOf(element)!= i){
+        delete iterable[i]
     }
-    //test by printing this in the console
-    console.log(duplicateEncode('BaRaban'));
+
+   });
+    result = iterable.filter((item)=>item)
+    console.log(result);
+    return result
+  }
+
+  uniqueInOrder([1,2,2,3,3])
--- task_05.js
+++ task_05.js
@@ -1,5 +1,6 @@
 var uniqueInOrder = function(it){
-    var arr = it.split('')
+    var arr
+    typeof it === 'string' ? arr = it.split(''): arr= it
     var newArr = []
     arr.forEach((char,idx) => {
         if ( idx !== arr.length && char !== arr[idx+1]) newArr.push(char)
